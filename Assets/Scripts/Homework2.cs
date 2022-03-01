e234567ba20c86ae37d20a948624a612beebec5a
mrak1444 <74318127+mrak1444@users.noreply.github.com>
2022-03-01 19:18:46 +0600

    Сompleted homework 2

---
+++ Assets/Scripts/Homework2.cs
@@ -0,0 +1,49 @@
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class Homework2 : MonoBehaviour
+{
+    void Start()
+    {
+        RunTask();
+    }
+
+    private async void RunTask()
+    {
+        CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+        CancellationToken cancelToken = cancelTokenSource.Token;
+
+        Task task1 = Task.Run(() => TaskSecond(cancelToken));
+        Task task2 = Task.Run(() => TaskFrames(cancelToken));
+        await Task.WhenAll(task1, task2);
+        Debug.Log("Все задачи выполнены");
+        cancelTokenSource.Cancel();
+        cancelTokenSource.Dispose();
+    }
+
+    async void TaskSecond(CancellationToken cancelToken)
+    {
+        int i = 1;
+        while (i < 1000)
+        {
+            if (cancelToken.IsCancellationRequested) Debug.Log("Прервано");
+            await Task.Delay(100);
+            i += 100;
+        }
+        Debug.Log("Одна секунда прошла");
+    }
+
+    async void TaskFrames(CancellationToken cancelToken)
+    {
+        int i = 1;
+        while (i < 60)
+        {
+            if (cancelToken.IsCancellationRequested) Debug.Log("Прервано");
+            await Task.Yield();
+            i++;
+        }
+
+        Debug.Log($"{i} кадров прошли");
+    }
+}
